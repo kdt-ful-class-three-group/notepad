3d9221ddd34f3313f19301fadc4000180a44478a
김재승 <jaes@gmail.com>
2025-02-17 16:27:20 +0900

    gg

--- app.js
+++ app.js
@@ -5,18 +5,21 @@
 
 const http = require("http");
 const fs = require("fs");
-
 const server = http.createServer(function (req, res) {
     let url = req.url;
     if (req.method === "GET") {
-        if (req.url === "/") {
+        if (url === "/") {
+            req.on("data", function () {
+                console.log(data);
+            });
             let main = fs.readFileSync("index.html");
+            console.log(url);
             res.writeHead(200, { "Content-Type": "text/html" });
             res.end(main);
         }
     }
 });
 
-server.listen(3000, () => {
-    console.log("3000서버 실행 http://localhost:3000");
+server.listen(3001, () => {
+    console.log("3000서버 실행 http://localhost:3001");
 });
